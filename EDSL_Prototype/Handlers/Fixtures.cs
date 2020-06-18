49177dc43ad43533bea152bf4b035725e7c90596
Jeditiger05 <john@jedimedia.it>
2020-06-18 21:28:28 +1000

    Draw Fixtures 1st Version

--- EDSL_Prototype/Handlers/Fixtures.cs
+++ EDSL_Prototype/Handlers/Fixtures.cs
@@ -10,33 +10,29 @@ namespace EDSL_Prototype.Handlers
 {
     class Fixtures
     {
-        class Fixture
-        {
-            public string Home { get; set; }
-            public string Away { get; set; }
-        }
-
         public static void CallCode(string divName)
         {
             Division division = DAFunctions.ReadDivision(divName);
             List<string> teams = DAFunctions.GetDivisionTeams(division.DivisionID);
             List<Round> rounds = new List<Round>();
-            List<Game> fixtures = CalculateFixtures(teams)
+            List<Game> fixtures = CalculateFixtures(teams);
             //var teams = new string[] { "A", "B", "C", "D" };
             //List<Fixture> fixtures = CalculateFixtures(teams);
         }
 
-        public static List<Fixture> CalculateFixtures(string[] teams)
+        public static List<Game> CalculateFixtures(List<string> teams)
         {
             //create a list of all possible fixtures (order not important)
-            List<Fixture> fixtures = new List<Fixture>();
-            for (int i = 0; i < teams.Length; i++)
+            List<Game> fixtures = new List<Game>();
+            for (int i = 0; i < teams.Count; i++)
             {
-                for (int j = 0; j < teams.Length; j++)
+                int gameNum = 1;
+                for (int j = 0; j < teams.Count; j++)
                 {
                     if (teams[i] != teams[j])
                     {
-                        fixtures.Add(new Fixture() { Home = teams[i], Away = teams[j] });
+                        fixtures.Add(new Game(gameNum, teams[j], teams[j], 0, 0));
+                        gameNum += 1;
                     }
                 }
             }
@@ -44,10 +40,10 @@ namespace EDSL_Prototype.Handlers
             fixtures.Reverse();//reverse the fixture list as we are going to remove element from this and will therefore have to start at the end
 
             //calculate the number of game weeks and the number of games per week
-            int gameweeks = (teams.Length - 1) * 2;
+            int gameweeks = (teams.Count - 1) * 2;
             int gamesPerWeek = gameweeks / 2;
 
-            List<Fixture> sortedFixtures = new List<Fixture>();
+            List<Game> sortedFixtures = new List<Game>();
 
             //foreach game week get all available fixture for that week and add to sorted list
             for (int i = 0; i < gameweeks; i++)
@@ -58,9 +54,9 @@ namespace EDSL_Prototype.Handlers
             return sortedFixtures;
         }
 
-        public static List<Fixture> TakeUnique(List<Fixture> fixtures, int gamesPerWeek)
+        public static List<Game> TakeUnique(List<Game> fixtures, int gamesPerWeek)
         {
-            List<Fixture> result = new List<Fixture>();
+            List<Game> result = new List<Game>();
 
             //pull enough fixture to cater for the number of game to play
             for (int i = 0; i < gamesPerWeek; i++)
@@ -69,7 +65,8 @@ namespace EDSL_Prototype.Handlers
                 for (int j = fixtures.Count - 1; j >= 0; j--)
                 {
                     //check to see if any teams in current fixtue have already been used this game week and ignore if they have
-                    if (!result.Any(r => r.Home == fixtures[j].Home || r.Away == fixtures[j].Home || r.Home == fixtures[j].Away || r.Away == fixtures[j].Away))
+                    if (!result.Any(r => r.HomeTeam == fixtures[j].HomeTeam || r.AwayTeam == fixtures[j].HomeTeam || 
+                    r.HomeTeam == fixtures[j].AwayTeam || r.AwayTeam == fixtures[j].AwayTeam))
                     {
                         //teams not yet used
                         result.Add(fixtures[j]);
